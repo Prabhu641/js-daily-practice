2511420284c25c4798f678b86623f5978c40849c
Prabhu R <prabhurprabhur063@gmail.com>
2025-05-04 09:18:04 +0530

    Day 1 – JS Basics: variables, conditionals

---
+++ day1.js
@@ -0,0 +1,10 @@
+// check voting if person is eligible to vote or not 
+
+let age=20;
+
+if(age>=18){
+    console.log("The Person is eligible to vote ")
+}
+else{
+    console.log("The Person is not eligible to vote")
+}
